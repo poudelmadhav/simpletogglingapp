05700007f4aa071fd0f4b70339e0ec6afdadaac7
Madhav Paudel <poudelmadhav143@gmail.com>
2017-04-16 10:03:54 +0545

    admin userface added

---
+++ app/admin/author.rb
@@ -0,0 +1,7 @@
+ActiveAdmin.register Author do
+  sidebar 'Books by this Author', :only => :show do
+    table_for Book.joins(:author).where(:author_id => author.id) do |t|
+      t.column("Title") { |book| book.name }
+    end
+  end
+end
