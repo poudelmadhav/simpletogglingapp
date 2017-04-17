a30cf1920f7e5a5367671d6718fa5315f41d8831
Madhav Paudel <poudelmadhav143@gmail.com>
2017-04-17 08:05:35 +0545

    permit params added to book, genre and author

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ end
 # use active admin
 gem 'activeadmin', github: 'activeadmin'
 
+# Import data
+gem 'active_admin_importable'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use sqlite3 as the database for Active Record
--- app/admin/author.rb
+++ app/admin/author.rb
@@ -1,7 +1,8 @@
 ActiveAdmin.register Author do
-  sidebar 'Books by this Author', :only => :show do
-    table_for Book.joins(:author).where(:author_id => author.id) do |t|
-      t.column("Title") { |book| book.name }
-    end
-  end
+	permit_params :first_name, :last_name
+	  sidebar 'Books by this Author', :only => :show do
+	    table_for Book.joins(:author).where(:author_id => author.id) do |t|
+	      t.column("Title") { |book| book.name }
+	    end
+	  end
 end
